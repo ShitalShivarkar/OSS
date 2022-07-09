97749910e3aca40f16e804a8bbe6c1bf2552c3af
ShitalShivarkar <108930327+ShitalShivarkar@users.noreply.github.com>
2022-07-09 16:20:53 +0530

    main Program file Added

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvFeatureDemoApp
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                //C# 3.0 : 
+                // annonymous Type
+                // partial Class
+                // extension method
+                // LinQ
+
+                //Annonymous Type
+                // ti is a dynamic type
+
+                var instance = new
+                {
+                    FirstName = "Shital",
+                    LastName = "Shivarkar",
+                    Age = 47,
+                    Email = "shital123@gmail.com",
+                    ContactNumber = "9898237896",
+                    CompanyOwner =false
+                };
+
+                Console.WriteLine(instance.FirstName + " " + instance.LastName + " Owner=" + instance.CompanyOwner);
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+
